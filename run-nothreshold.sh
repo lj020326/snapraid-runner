44e27c42502696cd680c2daf0402ad00119d9921
LJ Johnson <lj020326@gmail.com>
2021-03-11 11:02:43 -0500

    updates

--- run-nothreshold.sh
+++ run-nothreshold.sh
@@ -4,7 +4,8 @@
 
 ## cron:
 HC_URL=https://healthchecks.admin.johnson.int/ping/1c56e8f4-ed57-4b15-8a8e-d4009a878e6a
-CONF_PATH=/opt/snapraid-runner/snapraid-runner.conf
+#CONF_PATH=/opt/snapraid-runner/snapraid-runner.conf
+CONF_PATH=/opt/snapraid-runner/snapraid-runner.nodeletethreshold.conf
 RUN_SCRIPT=/opt/snapraid-runner/snapraid-runner.py
 
 python3 ${RUN_SCRIPT} -c ${CONF_PATH} && curl -fsS --retry 3 ${HC_URL} >/dev/null
